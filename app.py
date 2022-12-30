e647733b7e6c01cfa8a2866740eb03bbc17fc165
Dhanush Reddy <76517652+dhanushreddy291@users.noreply.github.com>
2022-12-30 22:25:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Web App with Python Flask!'
+def get_data():
+    data = {
+        'name': 'John',
+        'age': 30,
+        'city': 'New York'
+    }
+    return jsonify(data)
 
 if __name__ == '__main__':
    app.run()
